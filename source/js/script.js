c479c73b0ca00c706cce109f78291d28accfceaa
nookimy <nookimy@gmail.com>
2020-03-30 12:11:40 +0300

    Завершение верстки слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,10 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    dots: true,
+    autoplay: true, // автопрокрутка
+    autoplayTimeout: 5000, // задержка в мс
+    autoplayHoverPause: false, // не останавливать при наведении
+  });
 });
