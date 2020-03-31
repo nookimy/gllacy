bc1a6a340cc26b8efa20f6febe21db43bd725c36
nookimy <nookimy@gmail.com>
2020-03-31 17:32:54 +0300

    Начало верстки блока Контакты

--- dev/js/script.js
+++ dev/js/script.js
@@ -12,3 +12,22 @@ $(document).ready(function () {
 $( ".product--hit" )
   .parent()
   .addClass( "catalog__item--hit" );
+
+function initMap() {
+  var coordinates = {lat: 59.938872, lng: 30.323037},
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: coordinates,
+  }),
+
+  image = 'img/contacts/pin.svg',
+  marker = new google.maps.Marker({
+    position: coordinates,
+    map: map,
+    animation: google.maps.Animation.BOUNCE
+  });
+}
+
+
+
+
